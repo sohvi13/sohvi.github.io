7aa6d7542b1d8b02981ce0a0df7fa468a8793c12
sohvi13 <sohvi.haukkala@gmail.com>
2025-04-27 11:11:51 +0300

    Kartta tehtävä valmis

---
+++ Js/kartta.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    var defaultAddress = 'Yliopistonkatu 36';
+    var defaultCity = 'Lappeenranta';
+    var iframe = document.getElementById('mapframe');
+    iframe.src = 'https://www.google.com/maps?q=' +
+      encodeURIComponent(defaultAddress + ' ' + defaultCity) + '&output=embed';
+  };
+
+  // Haetaan elementit
+  var searchButton = document.getElementById('searchBtn');
+  searchButton.onclick = function() {
+    var address = document.getElementById('address').value;
+    var city = document.getElementById('city').value;
+    var query = address + ' ' + city;
+    var iframe = document.getElementById('mapframe');
+    iframe.src = 'https://www.google.com/maps?q=' +
+      encodeURIComponent(query) + '&output=embed';
+  };
